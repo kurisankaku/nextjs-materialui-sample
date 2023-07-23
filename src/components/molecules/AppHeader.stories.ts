d5afba59356ce720dd50bef7b51136b0ba6ae91f
kuriyama <kuriyama@ailes-dor.co.jp>
2023-07-23 14:58:10 +0900

    移動

--- src/components/molecules/AppHeader.stories.ts
+++ src/components/molecules/AppHeader.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import AppHeader from './AppHeader';
 
 const meta = {
-  title: 'organisms/AppHeader',
+  title: 'molecules/AppHeader',
   component: AppHeader,
   tags: ['autodocs'],
 } satisfies Meta<typeof AppHeader>;
