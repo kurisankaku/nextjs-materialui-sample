85fdf487f9d3883cc43898f988037e8dd3c2ec62
kuriyama <kuriyama@ailes-dor.co.jp>
2023-07-23 14:07:45 +0900

    StorybookとMaterialUIの統合

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,11 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@storybook/addon-styling',
+    {
+      name: '@storybook/addon-styling',
+      options: {},
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
