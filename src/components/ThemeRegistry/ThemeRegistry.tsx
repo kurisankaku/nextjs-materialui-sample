8fc7a1bcc22124bc83795488677b557092d133a8
kurisankaku <kurisankaku@gmail.com>
2023-08-06 22:32:05 +0900

    DatePickerの導入

--- src/components/ThemeRegistry/ThemeRegistry.tsx
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -3,16 +3,25 @@
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import ja from 'date-fns/locale/ja';
 import NextAppDirEmotionCacheProvider from './EmotionCache';
 import theme from './theme';
 
-export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
+export default function ThemeRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <NextAppDirEmotionCacheProvider options={{ key: 'mui' }}>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        {children}
+        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ja}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {children}
+        </LocalizationProvider>
       </ThemeProvider>
     </NextAppDirEmotionCacheProvider>
   );
